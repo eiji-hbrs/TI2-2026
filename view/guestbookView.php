bdaebb0f5086647a62ae44d21a95d9f2a45de3d5
eiji-hbrs <eiji.haubursin@outlook.com>
2026-05-27 12:12:12 +0200

    début de CSS

--- view/guestbookView.php
+++ view/guestbookView.php
@@ -17,63 +17,64 @@
 <body>
     <header>
         <img class="logo" src="./img/logo.png" alt="logo">
-        <h1  class="titre">TI2 | Livre d'or</h1>
-        <p class="sousTitre">Laissez une trace de passage !</p>
-        <div>
+        <h1 class="titre">TI2 | Livre d'or</h1>
+        <div class="rouage">
             <img class="settings" src="./img/settings.png" alt="settings">
-            <span>Administion</span>
+            <span id="admin">Administion</span>
         </div>
 
     </header>
     <!-- Formulaire d'ajout d'un message -->
-
-    <img class="book" src="./img/book.png" alt="livre">
-
-    <h2>Votre message</h2>
+    <div class="livre">
+    <img class="book" src="./img/livre-ouvert.png" alt="livre">
+    </div>
     <div class="formulaire">
-        <form method="POST" action="">
+        <h2>Votre message</h2>
         
-            <div class="form-group">
-                <label for="firstname">Prénom</label>
-                <input type="text" name="firstname" id="firstname"
-                    value="<?= htmlspecialchars($_POST['firstname'] ?? '') ?>" required>
-            </div>
+            <form method="POST" action="">
 
-            <div class="form-group">
-                <label for="lastname">Nom</label>
-                <input type="text" name="lastname" id="lastname"
-                    value="<?= htmlspecialchars($_POST['lastname'] ?? '') ?>" required>
-            </div>
-
-            <div class="form-group">
-                <label for="usermail">E-mail</label>
-                <input type="email" name="usermail" id="usermail"
-                    value="<?= htmlspecialchars($_POST['usermail'] ?? '') ?>" required>
-            </div>
-
-            <div class="form-group">
-                <label for="postcode">Code Postal</label>
-                <input type="text" name="postcode" id="postcode"
-                    value="<?= htmlspecialchars($_POST['postcode'] ?? '') ?>" required>
-            </div>
-
-            <div class="form-group">
-                <label for="phone">Numero de Téléphone</label>
-                <input type="text" name="phone" id="phone" value="<?= htmlspecialchars($_POST['phone'] ?? '') ?>"
-                    required>
-            </div>
-
-            <div class="form-group">
-                <label for="message">Message</label>
-                <div class="textarea-wrapper">
-                    <textarea name="message" id="message" rows="5"
-                        required><?= htmlspecialchars($_POST['message'] ?? '') ?></textarea>
-                    <span id="charCount" class="char-count">0 / 300 caractères</span>
+                <div class="form-group">
+                    <label for="firstname">Prénom</label>
+                    <input type="text" name="firstname" id="firstname"
+                        value="<?= htmlspecialchars($_POST['firstname'] ?? '') ?>" required>
                 </div>
-            </div>
 
-            <button type="submit" class="submit-btn" id="signupBtn">Envoyer le message</button>
-    </form>
+                <div class="form-group">
+                    <label for="lastname">Nom</label>
+                    <input type="text" name="lastname" id="lastname"
+                        value="<?= htmlspecialchars($_POST['lastname'] ?? '') ?>" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="usermail">E-mail</label>
+                    <input type="email" name="usermail" id="usermail"
+                        value="<?= htmlspecialchars($_POST['usermail'] ?? '') ?>" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="postcode">Code Postal</label>
+                    <input type="text" name="postcode" id="postcode"
+                        value="<?= htmlspecialchars($_POST['postcode'] ?? '') ?>" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="phone">Numero de Téléphone</label>
+                    <input type="text" name="phone" id="phone" value="<?= htmlspecialchars($_POST['phone'] ?? '') ?>"
+                        required>
+                </div>
+
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <div class="textarea-wrapper">
+                        <textarea name="message" id="message" rows="5"
+                            required><?= htmlspecialchars($_POST['message'] ?? '') ?></textarea>
+                        <span id="charCount" class="char-count">0 / 300 caractères</span>
+                    </div>
+                </div>
+
+                <button type="submit" class="submit-btn" id="signupBtn">Envoyer le message</button>
+            </form>
+    </div>    
     <?php
     // on a tenté d'envoyé le formulaire et
     // il a passé les protections frontend
@@ -118,12 +119,13 @@
             <?php
         else:
             ?>
-            <h2>Messages récents - il y a actuellement (<?= $nbMessages ?>) messages</h2>
+            <h2>Messages récents - il y a actuellement <?= $nbMessages ?> messages</h2>
             <?php
             foreach ($messages as $message):
                 ?>
                 <div class="message-card">
-                    <h3>Ecrit par <?= htmlspecialchars($message['firstname']) ?> le
+                    <h3>Ecrit par <?= htmlspecialchars($message['firstname']) ?>         <?= htmlspecialchars($message['lastname']) ?>
+                        le
                         <?= htmlspecialchars($message['datemessage']) ?>
                     </h3>
                     <p><?= nl2br(htmlspecialchars($message['message'])) ?></p>
@@ -140,12 +142,12 @@
     <!-- Pagination (BONUS) -->
     <?php
     // À commenter quand on a fini de tester
-    echo "<h3>Nos var_dump() pour le débugage</h3>";
-    echo '<p>$_POST</p>';
-    var_dump($_POST);
-    echo '<p>$_GET</p>';
-    var_dump($_GET);
-    ?>
+    // echo "<h3>Nos var_dump() pour le débugage</h3>";
+    // echo '<p>$_POST</p>';
+    // var_dump($_POST);
+    // echo '<p>$_GET</p>';
+    // var_dump($_GET);
+    // ?>
 
     <script src="js/validation.js"></script>
 </body>
